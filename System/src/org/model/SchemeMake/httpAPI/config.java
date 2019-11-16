d77b89983438d2e1945e8d78752a151565c9e5e2
iWannaName <1183110578@qq.com>
2019-11-16 16:51:03 +0800

    delete file

--- System/src/org/model/SchemeMake/httpAPI/config.java
+++
@@ -1,7 +0,0 @@
-package org.model.SchemeMake.httpAPI;
-
-import org.model.settings;
-
-public class config {
-	public static String AK = settings.AK; //百度地图 AK
-}
--- System/src/org/model/policeman.java
+++
@@ -1,6 +0,0 @@
-package org.model;
-
-public class policeman extends user {
-	
-	
-}
